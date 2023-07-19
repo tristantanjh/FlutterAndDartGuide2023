febe2957b8adc2163c04e11da7b4dc4586c5fcf8
tris <tristantanjh@gmail.com>
2023-07-19 16:38:50 +0800

    Add Dice Image and Roll Dice Button

--- dice_game/lib/gradient_container.dart
+++ dice_game/lib/gradient_container.dart
@@ -9,19 +9,38 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colorsList;
 
+  void rollDice() {
+    //
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: colorsList,
-          begin: startAlignment,
-          end: endAlignment,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colorsList,
+            begin: startAlignment,
+            end: endAlignment,
+          ),
         ),
-      ),
-      child: const Center(
-        child: StyledText('Hello World!'),
-      ),
-    );
+        child: Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            // const SizedBox(height: 20), // Add Box of size 20 to simulate padding
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                  ), // Add Padding
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28)),
+              child: const Text('Roll Dice'),
+            )
+          ]),
+        ));
   }
 }
