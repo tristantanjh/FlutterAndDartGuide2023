97f8f116ff8aa7e536dd71e7927039b66f75b479
tris <tristantanjh@gmail.com>
2023-07-22 15:29:27 +0800

    Initial Commit and Make Home Page for Quiz

---
+++ quiz_app/lib/background.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  const Background({super.key});
+
+  void onPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const Padding(
+              padding: EdgeInsets.all(40),
+              child: Text('Learn Flutter the Fun way!',
+                  style: TextStyle(color: Colors.white, fontSize: 28))),
+          OutlinedButton.icon(
+              onPressed: onPressed,
+              style: OutlinedButton.styleFrom(
+                  padding: const EdgeInsets.all(10),
+                  foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right),
+              label: const Text('Start Quiz')),
+        ],
+      ),
+    );
+  }
+}
