1da5c892a78386de30f4c660a4369e8e752032c8
tris <tristantanjh@gmail.com>
2023-07-24 15:59:28 +0800

    Format expense entries

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
 const uuid = Uuid();
+final formatter = DateFormat.yMd();
 
 enum Category {
   food,
@@ -8,6 +11,13 @@ enum Category {
   leisure,
   work,
 }
+
+const categoryIcons = {
+  Category.food: Icons.fastfood,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.chair,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -22,4 +32,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -9,7 +10,8 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+        // to dynamically generate long lists
         itemCount: expenseList.length,
-        itemBuilder: (ctx, index) => Text(expenseList[index].title));
+        itemBuilder: (ctx, index) => ExpensesItem(expenseList[index]));
   }
 }
