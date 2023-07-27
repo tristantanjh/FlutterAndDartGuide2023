75e465bb0976ddafcb248859a614b8d43a702afa
tris <tristantanjh@gmail.com>
2023-07-27 16:17:56 +0800

    Add categories with styling

---
+++ meals_app/lib/screens/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Choose your category!'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(8),
+        // this delegate means num of col is fixed
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // num of cols
+          childAspectRatio: 3 / 2, // height : width
+          crossAxisSpacing: 20, // horizontal spacinng
+          mainAxisSpacing: 20, // vertical spacing
+        ),
+        children: [for (final c in availableCategories) CategoryGridItem(c)],
+      ),
+    );
+  }
+}
---
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -0,0 +1,37 @@
+import 'package:meals_app/models/category.dart';
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem(this.category, {super.key});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
