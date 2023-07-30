e5fe3656d9e79b6f4ab87fa814a1755e8aa2cd97
tris <tristantanjh@gmail.com>
2023-07-30 21:32:25 +0800

    Add basic ui

--- shopping_list/lib/main.dart
+++ shopping_list/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: ...,
+      home: const GroceryList(),
     );
   }
 }
