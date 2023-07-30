715f2111de1bfda90a6f39641f50ad9d575d8bfc
tris <tristantanjh@gmail.com>
2023-07-30 17:32:18 +0800

    Create models

--- shopping_list/lib/data/dummy_items.dart
+++ shopping_list/lib/data/dummy_items.dart
@@ -1,7 +1,8 @@
 import 'package:shopping_list/models/grocery_item.dart';
-import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/data/category_data.dart';
+import 'package:shopping_list/models/category.dart';
 
-const groceryItems = [
+final groceryItems = [
   GroceryItem(
       id: 'a',
       name: 'Milk',
