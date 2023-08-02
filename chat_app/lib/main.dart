ef8405b8eda00a0346780116b6294d3d5ec0c640
tris <tristantanjh@gmail.com>
2023-08-02 16:26:54 +0800

    Add splash screen and logout function

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -28,11 +29,15 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
           if (snapshot.hasData) {
             return const ChatScreen();
-          } else {
-            return const AuthScreen();
           }
+
+          return const AuthScreen();
         },
       ),
     );
