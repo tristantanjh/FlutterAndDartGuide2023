0d601d9790bc969b50aa4f073681abcba285c47e
tris <tristantanjh@gmail.com>
2023-08-02 15:11:21 +0800

    Create basic login screen

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: ...
+      home: const AuthScreen(),
     );
   }
 }
